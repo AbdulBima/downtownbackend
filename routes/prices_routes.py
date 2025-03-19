ae08cfa0230896b4bf08c038a62608df57d1864a
Abdul Bima <abdulbimadev@gmail.com>
2025-03-19 06:11:26 +0100

    major upd2

--- routes/prices_routes.py
+++ routes/prices_routes.py
@@ -13,13 +13,30 @@ collection = db.price_settings_collection  # Collection to store the price setti
 
 @router.get("/getprices", response_model=PriceSettingsSerializer)
 async def get_price_settings():
-    """
-    Fetch the current price settings.
-    If no settings exist, initialize with default values.
-    """
     settings_doc = await collection.find_one({})
-    
+
+    if not settings_doc:
+        # Return default values
+        settings_doc = {
+            "company_prices": {
+                "recyclingKg": 0.0,
+                "blendingKg": 0.0,
+                "crushingKg": 0.0
+            },
+            "labour_prices": {
+                "recycling": 0.0,
+                "blending": 0.0,
+                "crushingWaste": 0.0,
+                "crushingSack": 0.0
+            }
+        }
+    else:
+        # Remove MongoDB _id if present
+        settings_doc.pop("_id", None)
+
     return settings_doc
+
+
 
 @router.put("/putprices", response_model=PriceSettingsSerializer)
 async def update_price_settings(settings: PriceSettingsSerializer):
